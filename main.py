be4e9ef3cfbd6588e8fb4c7697ef662963c1343a
Nicolas <nicolasmader001@gmail.com>
2026-03-18 13:37:10 +0100

    add: player can guess letters, win detects, can't duplicate guesses

--- main.py
+++ main.py
@@ -7,28 +7,44 @@ word = random.choice(word_liste)
 
 guessed_letters = []
 
-
+#funkton
 def hangman():
 
     print("Viel Spaß mit dem Spiel Galgenmännchen")
 
     while True :
-        letter = input("Rate einen Buchstaben:")
-        guessed_letters.append(letter)
+        user_letter = input("\n""Rate einen Buchstaben: ")
 
-        if letter in word:
+        # verhindert doppelte eingaben
+        if user_letter in guessed_letters:
+            print("Die Buchsaben ",guessed_letters, "hast du schon benutzt")
+            continue
+        else:
+            guessed_letters.append(user_letter)
+
+        #rückmeldung richtig falsch
+        if user_letter in word:
             print("Richtig")
-
         else:
             print("Falsch")
 
-        for letter in word:
-            if letter in guessed_letters:
-                print(letter,end="")    #end="" verhindert die zeilen brüche von print
+        #wort anzeige
+        for word_letter in word:
+            if word_letter in guessed_letters:
+                print(word_letter,end="")    #end="" verhindert die zeilenumbrüche von print
 
             else :
                 print("_",end="")
+        #prüfung ob gewonnen
+        win = True
+        for letter in word:
+            if letter not in guessed_letters:
+                win = False
+                break
+        if win :
+            print("\n""Gewonnen du hast das Wort ", word, "erraten")
+            break
 
-
+#funktion starten
 hangman()
 
